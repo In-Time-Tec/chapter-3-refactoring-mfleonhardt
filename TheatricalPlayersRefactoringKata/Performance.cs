de56c9736cc1870aa8e86b8153b817e4069f9b91
Matt Leonhardt <matt.leonhardt@fullbay.com>
2022-12-13 06:01:31 -0700

    Inject Play directly into Performance, add audience and comedy credit attributes to Performance object

--- TheatricalPlayersRefactoringKata/Performance.cs
+++ TheatricalPlayersRefactoringKata/Performance.cs
@@ -2,17 +2,30 @@ namespace TheatricalPlayersRefactoringKata
 {
     public class Performance
     {
-        private string _playID;
         private int _audience;
+        private int _cost;
+        private int _comedyCredit;
+        private Play _play;
 
-        public string PlayID { get => _playID; set => _playID = value; }
         public int Audience { get => _audience; set => _audience = value; }
+        public int Cost { get => _cost; set => _cost = value; }
+        public int ComedyCredit { get => _comedyCredit; set => _comedyCredit = value; }
+        public Play Play { get => _play; set => _play = value; }
 
-        public Performance(string playID, int audience)
+        public Performance(Play play, int audience)
         {
-            this._playID = playID;
+            this._play = play;
             this._audience = audience;
         }
 
+        public void CalculateCost()
+        {
+            _cost  = _play.CalculateCost(_audience);
+        }
+
+        public void CalculateComedyCredit()
+        {
+            _comedyCredit = _play.CalculateComedyCredit(_audience);
+        }
     }
 }
